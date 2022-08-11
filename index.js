d5cf7da9b70fbab0b40e4c929e8281513e675261
kusi <lawcubegsm@gmail.com>
2022-08-11 14:00:40 +0000

    Add form style

--- index.js
+++ index.js
@@ -2,84 +2,91 @@ const bookForm = document.getElementById('book-form');
 const titleForm = bookForm.elements['title-input'];
 const authorForm = bookForm.elements['author-input'];
 const bookList = document.getElementById('books-list');
-const navList = document.getElementById('nav-list');
-const navAdd = document.getElementById('nav-add');
-const navContact = document.getElementById('nav-contact');
-const listContainer = document.querySelector('.list-container');
-const formContainer = document.querySelector('.form-container');
-const contactInfo = document.querySelector('.contact-info');
 
 class Book {
-	booksArray = [];
+	 booksArray = [];
 
-	constructor() {
-		this.getLocalStorage();
-	}
+  constructor() {
+   this.getLocalStorage();
+  }
 
-	inArray(id) {
-		this.booksArray.forEach((element) => {
-			if (element.Title.includes(id)) {
-				return true;
-			}
-			return false;
-		});    
-	}
+  inArray(id) {
+   this.booksArray.forEach((element) => {
+    if (element.Title.includes(id)) {
+     return true;
+    }
+    return false;
+   });    
+  }
 
-	addBookToArray(title, author) {
-		// Check for empty book and add book to booksArray
-		if (title && author) {
-			if (!this.inArray(title)) {
-				this.booksArray = [...this.booksArray, {
-					id: title,
-					Title: title,
-					Author: author,
-				}];
-			}
-		}
-	}
+  addBookToArray(title, author) {
+   // Check for empty book and add book to booksArray
+   if (title && author) {
+    if (!this.inArray(title)) {
+     this.booksArray = [...this.booksArray, {
+      id: title,
+      Title: title,
+      Author: author,
+     }];
+    }
+   }
+  }
 
-	displayPage() {
-		bookList.innerHTML = '';
-		this.booksArray.forEach(Book.addToPage);
-	}
+  displayPage() {
+   bookList.innerHTML = '';
+   this.booksArray.forEach(Book.addToPage);
+  }
 
-	static addToPage(book) {
-		bookList.innerHTML += `
-		<tr class="table-row" >
-			<td class="title-col" >"${book.Title}" by ${book.Author}</td>
-			<td class="btn-col"><button id="${book.Title}" type="submit" onclick= book.removeBook(this.id)>Remove</button></td>
-		</tr>
-		`;
-	}
+  static addToPage(book) {
+   bookList.innerHTML += `
+   <tr class="table-row" >
+    <td class="title-col" >"${book.Title}" by ${book.Author}</td>
+    <td class="btn-col"><button id="${book.Title}" type="submit" onclick= book.removeBook(this.id)>Remove</button></td>
+   </tr>
+   `;
+  }
 
-	// Update booksArray with data from localStorage
-	getLocalStorage() {
-		// Check if data is in storage and convert to js object
-		if (localStorage.getItem('bookList')) {
-			this.booksArray = JSON.parse(localStorage.getItem('bookList'));
-		}
-	}
+  // Update booksArray with data from localStorage
+  getLocalStorage() {
+   // Check if data is in storage and convert to js object
+   if (localStorage.getItem('bookList')) {
+    this.booksArray = JSON.parse(localStorage.getItem('bookList'));
+   }
+  }
 
-	setLocalStorage() {
-		localStorage.setItem('bookList', JSON.stringify(this.booksArray));
-	}
+  setLocalStorage() {
+   localStorage.setItem('bookList', JSON.stringify(this.booksArray));
+  }
 
-	removeBook(title) {
-		this.booksArray = this.booksArray.filter((book) => book.Title !== title);
-		bookList.innerHTML = '';
-		this.setLocalStorage('bookList', this.booksArray);
-		this.displayPage();
-	}
+  removeBook(title) {
+   this.booksArray = this.booksArray.filter((book) => book.Title !== title);
+   bookList.innerHTML = '';
+   this.setLocalStorage('bookList', this.booksArray);
+   this.displayPage();
+   console.log('I have been clicked')
+  }
 }
+
+const contact = document.querySelector('#nav-contact')
+const navList = document.getElementById('nav-list');
+const navAdd = document.getElementById('nav-add');
+const listContainer = document.querySelector('.list-container');
+const formContainer = document.querySelector('.form-container');
+const contactContainer=document.querySelector('.contact-container');
+
 
 const book = new Book();
 
+
 window.addEventListener('load', () => {
 	book.displayPage();
+ formContainer.style.display = 'none';
+ contactContainer.style.display = 'none';
 });
 
 bookForm.addEventListener('submit', (e) => {
 	e.preventDefault();
+ console.log(e.target.id)
 	const title = titleForm.value;
 	const author = authorForm.value;
 
@@ -92,32 +99,37 @@ bookForm.addEventListener('submit', (e) => {
 });
 
 navList.addEventListener('click', (e)=> {
-	e.preventDefault(); 
-	formContainer.style.display = 'none'; 
-	contactInfo.style.display = 'none';
-	listContainer.style.display = 'flex';
-	book.displayPage();
+  e.preventDefault(); 
+  console.log(e.target.className)
+  formContainer.style.display = 'none'; 
+  contactContainer.style.display = 'none';
+  listContainer.style.display = 'flex';
+  book.displayPage();
 }); 
 
 navAdd.addEventListener('click', (e)=> {	
 	e.preventDefault(); 
-	contactInfo.style.display = 'none';
+ console.log(e.target.id)
+	contactContainer.style.display = 'none';
 	listContainer.style.display = 'none'; 
 	formContainer.style.display = 'flex';
 	formContainer.style.flexDirection = 'column'; 
 }); 
 
-contactInfo.addEventListener('click', (e)=> {
-	e.preventDefault(); 
+contact.addEventListener('click', (e)=> {	
+ contactContainer.innerHTML = ""
 	formContainer.style.display = 'none'; 
 	listContainer.style.display = 'none'; 
-	contactInfo.style.display = 'flex';	
-	contactInfo.innerHTML = `
+	contactContainer.style.display = 'flex';	
+ let div = document.createElement('div')
+ div.className = "info-wrapper"
+	div.innerHTML = `
 	<h2 class="contact-heading">Contact Information</h2>
 	<p class="contact-paragraph">If you have any questions or you want to say "Hello",</p>
 	<p class="contact-paragraph">you can reach out to us @ </p>
 	<p class="email">info@awesomebooks.com</p>
 	<p class="phone">+54367869011</p>
-	` 
-	
-}); 
+	`
+ contactContainer.appendChild(div)
+ e.preventDefault(); 
+});
